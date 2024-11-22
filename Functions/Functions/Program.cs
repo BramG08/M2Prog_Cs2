ba078e98ac02be3e758c17ec926fe3416c8992ac
BramG08 <bgrippeling08@gmail.com>
2024-11-22 10:02:36 +0100

    functions 3

---
+++ Functions/Functions/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Functions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Run();
+        }
+
+        internal void Run()
+        {
+            Vraag1();
+            Vraag2();
+            Vraag3();
+            Vraag4();
+            Vraag5();
+            Vraag6();
+
+        }
+        internal void Vraag1()
+        {
+            Console.WriteLine("How long do you think you'd survive in a zombie apocalypse?");
+            string antwoord = Console.ReadLine();
+            Console.WriteLine(antwoord);
+        }
+        internal void Vraag2()
+        {
+            Console.WriteLine("The funniest thing that happened in your life?");
+            string antwoord = Console.ReadLine();
+            Console.WriteLine(antwoord);
+        }
+
+        internal void Vraag3()
+        {
+            Console.WriteLine("what do you call a male ladybug?");
+            string antwoord = Console.ReadLine();   
+            Console.WriteLine(antwoord);
+        }
+         internal void Vraag4()
+        {
+            Console.WriteLine("Why do round pizzas come in square boxes?");
+            string antwoord = Console.ReadLine();   
+            Console.WriteLine(antwoord);
+        }
+
+        internal void Vraag5() 
+        { 
+            Console.WriteLine("Why are you friends with your friends");
+            string antwoord = Console.ReadLine();   
+            Console.WriteLine(antwoord);
+        }
+        internal void Vraag6() 
+        {
+            Console.WriteLine("How is your family?");
+            string antwoord = Console.ReadLine();   
+            Console.WriteLine(antwoord);
+        }
+    }
+}
