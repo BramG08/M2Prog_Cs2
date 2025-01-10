7e106fa6128de0dcc77ab05375710cf138aee873
BramG08 <bgrippeling08@gmail.com>
2025-01-10 10:11:46 +0100

    finsihed 00
    finsihed 04 lists herhaling.md

---
+++ lists04/lists04/Formulier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lists04
+{
+    internal class Formulier
+    {
+        internal int Sterrren {  get; set; }
+
+        internal string Feedback { get; set; }
+
+    }
+    formulieren[0] = new Formulier()
+    {
+        Feedback = "prijzig maar wel lekker",
+        Sterren = 5
+    };
+}
---
+++ lists04/lists04/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace lists04
+{
+    internal class Program
+    {
+        double[] prijzen = new double[] { 0.99, 5.60, 10.11 };
+        string[] artiekelen = { "snoepje", "luxe broodje", "lunch menu" };
+        Formulier[] formulieren = new Formulier[2];
+    }
+}
