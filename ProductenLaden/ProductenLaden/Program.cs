db39876954440e265f0af3f2b66ee782a75c9dbb
BramG08 <bgrippeling08@gmail.com>
2025-01-24 10:56:42 +0100

    verder gegaan met 3 maar ni af

---
+++ ProductenLaden/ProductenLaden/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.Json;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ProductenLaden
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Run();
+        }
+        static void RunProducten(string[] args)
+        {
+            Program program = new Program();
+            program.RunProducten();
+        }
+        internal void RunProducten() 
+        {
+            string text = File.ReadAllText("Producten.json");
+            Product[] product = JsonSerializer.Deserialize<Product[]>(text);
+          
+            Console.WriteLine(product);
+
+            for (int i = 0; i < product.Length; i++) 
+            {
+                Console.WriteLine(product[i]);
+            }
+    
+        }
+        internal void Run()
+        {
+            string text = File.ReadAllText("Product.json");
+            Product product = JsonSerializer.Deserialize<Product>(text);
+
+            Console.WriteLine(product.Name);
+            Console.WriteLine(product.Description);
+            Console.WriteLine(product.Price);
+
+
+           
+        }
+    }
+}
