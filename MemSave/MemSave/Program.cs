a37fbe7663582585a1a71e4eeac326ff55037fc3
BramG08 <bgrippeling08@gmail.com>
2025-01-24 10:23:16 +0100

    02 done

---
+++ MemSave/MemSave/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace MemSave
+{
+    internal class Program
+    {
+        string saveFile = "welkomState.txt";
+        string welkomsTekst = "hello world";
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Run();
+        }
+        internal void Run() 
+        {
+            bool bestaatDeFile = File.Exists(saveFile);
+            if (bestaatDeFile == true)
+            {
+                welkomsTekst = File.ReadAllText(saveFile);
+                Console.WriteLine(welkomsTekst);
+            }
+            
+            while (true) 
+            {
+                Console.WriteLine(welkomsTekst);
+                Console.WriteLine("Enter a text ,then press enter");
+                welkomsTekst = Console.ReadLine();
+                File.WriteAllText(saveFile, welkomsTekst);
+                
+            }
+        }
+    }
+}
