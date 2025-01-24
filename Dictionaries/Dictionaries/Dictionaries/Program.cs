f8d6b8fa943cef5cc800fea10f00df4520335e28
BramG08 <bgrippeling08@gmail.com>
2025-01-24 09:02:12 +0100

    updated

--- Dictionaries/Dictionaries/Dictionaries/Program.cs
+++ Dictionaries/Dictionaries/Dictionaries/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 
 namespace Dictionaries
 {
     internal class Program
     {
+        Dictionary<string, Room> Rooms = new Dictionary<string, Room>();
         static void Main(string[] args)
         {
             Program program = new Program();
@@ -28,6 +30,52 @@ namespace Dictionaries
             foreach (string value in personeel.Values) 
             {
                 Console.WriteLine(value);
+            }
+            Room start = new Room(0, 0, [
+            "xxxxx",
+            "x...x",
+            "x...x",
+            "x...x",
+            "xxxxx"
+            ]);
+            Room room0 = new Room(0, 1, [
+            "xxxxx",
+            "x...x",
+            "xxxxx",
+            "x...x",
+            "xxxxx", ]);
+            Room room1 = new Room(1, 1, [
+            "xxxxx",
+            "x...x",
+            "x.X.x",
+            "x...x",
+            "xxxxx",
+            ]);
+            Room room2 = new Room(1, 0, [
+            "xxxxx",
+            "xx..x",
+            "x...x",
+            "x..xx",
+            "xxxxx",
+            ]);
+            Rooms.Add(start.GetRoomLocationKey(), start);
+            Rooms.Add(room0.GetRoomLocationKey(), room0);
+            Rooms.Add(room1.GetRoomLocationKey(), room1);
+            Rooms.Add(room2.GetRoomLocationKey(), room2);
+            ShowRoom(0, 0);
+            ShowRoom(0, 1);
+            ShowRoom(1, 0);
+            ShowRoom(1, 1);
+             
+        }
+        private void ShowRoom(int x, int y) 
+        {
+            Room room = Rooms[$"{x},{y}"];
+
+            Console.WriteLine($"You are in room {x},{y}");
+            foreach (string r in room.level)
+            {
+                Console.WriteLine(r);
             }
         }
 
