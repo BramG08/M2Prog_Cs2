ad08aa7e535b5f5307446de3fa2a013f6f130c82
BramG08 <bgrippeling08@gmail.com>
2025-01-24 09:23:14 +0100

    Updated

--- Dictionaries/Dictionaries/Dictionaries/Program.cs
+++ Dictionaries/Dictionaries/Dictionaries/Program.cs
@@ -1,83 +1,113 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System;
+using System.Collections.Generic;
 
-namespace Dictionaries
+namespace RoomDictionaryExample
 {
-    internal class Program
+    public class Room
     {
-        Dictionary<string, Room> Rooms = new Dictionary<string, Room>();
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string[] Layout { get; set; }
+
+        public Room(string name, string description, string[] layout)
+        {
+            Name = name;
+            Description = description;
+            Layout = layout;
+        }
+
+
+        internal string GetRoomLocationKey(int x, int y)
+        {
+            return $"{x},{y}";
+        }
+    }
+
+    class Program
+    {
+
+        private static Dictionary<string, Room> rooms = new Dictionary<string, Room>();
         static void Main(string[] args)
         {
-            Program program = new Program();
-            program.Run();
-        }
-        internal void Run()         
-        { 
-            Dictionary<int, string> personeel = new Dictionary<int, string>();
-            personeel.Add(382942, "Jermaine");
-            personeel.Add(789432, "Timothy");
 
-        
+            Room startRoom = new Room(
+                "Start Room",
+                "This is the starting room.",
+                new string[]
+                {
+                    "xxxxx",
+                    "x...x",
+                    "x...x",
+                    "xxxxx"
+                });
+            rooms.Add(startRoom.GetRoomLocationKey(0, 0), startRoom);
 
-            foreach (KeyValuePair<int, string> item in personeel)
-            {
-                Console.WriteLine(item.Value + ": "+ item.Key);
-            }
-            foreach (int key in personeel.Keys)
-            {
-                Console.WriteLine(key);
-            }
-            foreach (string value in personeel.Values) 
-            {
-                Console.WriteLine(value);
-            }
-            Room start = new Room(0, 0, [
-            "xxxxx",
-            "x...x",
-            "x...x",
-            "x...x",
-            "xxxxx"
-            ]);
-            Room room0 = new Room(0, 1, [
-            "xxxxx",
-            "x...x",
-            "xxxxx",
-            "x...x",
-            "xxxxx", ]);
-            Room room1 = new Room(1, 1, [
-            "xxxxx",
-            "x...x",
-            "x.X.x",
-            "x...x",
-            "xxxxx",
-            ]);
-            Room room2 = new Room(1, 0, [
-            "xxxxx",
-            "xx..x",
-            "x...x",
-            "x..xx",
-            "xxxxx",
-            ]);
-            Rooms.Add(start.GetRoomLocationKey(), start);
-            Rooms.Add(room0.GetRoomLocationKey(), room0);
-            Rooms.Add(room1.GetRoomLocationKey(), room1);
-            Rooms.Add(room2.GetRoomLocationKey(), room2);
-            ShowRoom(0, 0);
+
+            Room room1 = new Room(
+                "Room 1",
+                "This is another room.",
+                new string[]
+                {
+                    "xxxxx",
+                    "x...x",
+                    "x.x.x",
+                    "xxxxx"
+                });
+            rooms.Add(room1.GetRoomLocationKey(1, 0), room1);
+
+            Room room2 = new Room(
+                "Room 2",
+                "This room is to the north.",
+                new string[]
+                {
+                    "xxxxx",
+                    "x...x",
+                    "x.X.x",
+                    "xxxxx"
+                });
+            rooms.Add(room2.GetRoomLocationKey(0, 1), room2);
+
+            Room room3 = new Room(
+                "Room 3",
+                "A room to the east.",
+                new string[]
+                {
+                    "xxxxx",
+                    "x...x",
+                    "x...x",
+                    "xxxxx"
+                });
+            rooms.Add(room3.GetRoomLocationKey(1, 1), room3);
+
+
             ShowRoom(0, 1);
-            ShowRoom(1, 0);
             ShowRoom(1, 1);
-             
+            ShowRoom(1, 0);
+            ShowRoom(2, 2);
         }
-        private void ShowRoom(int x, int y) 
+
+
+        private static void ShowRoom(int x, int y)
         {
-            Room room = Rooms[$"{x},{y}"];
+            string key = $"{x},{y}";
 
-            Console.WriteLine($"You are in room {x},{y}");
-            foreach (string r in room.level)
+            if (rooms.ContainsKey(key))
             {
-                Console.WriteLine(r);
-            }
-        }
+                Room room = rooms[key];
 
+                foreach (string line in room.Layout)
+                {
+                    Console.WriteLine(line);
+                }
+
+                Console.WriteLine($"You are in {room.Name}");
+            }
+            else
+            {
+                Console.WriteLine("Error room does not exist");
+            }
+
+            Console.WriteLine();
+        }
     }
 }
