64456c031f752d41dcc97f0e16e8b35f1283f7e3
BramG08 <bgrippeling08@gmail.com>
2025-01-27 12:39:10 +0100

    Schriven updated maar niet af

---
+++ Schrijven/Schrijven/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Schrijven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Run();
+        }
+        internal void Run()
+        {
+
+        }
+
+    }
+}
